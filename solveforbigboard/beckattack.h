581e00009d5d398d8eeaabdf31e3ee929459d03c
egekabas <egekabas@gmail.com>
2021-06-26 17:39:32 +0300

    Added algorithm to find winning cases for the second player using Beck's theorems

---
+++ solveforbigboard/beckattack.h
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ull, ull> pull;
+typedef pair<int, int> pii;
+typedef pair<ld, ld> pld;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+
+}
